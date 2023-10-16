7c29c444d4d379e9dc6b375a716a39fa77593d9f
Tim Huitt <timhuitt@gmail.com>
2023-10-16 00:23:41 -0600

    final

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+// Content Creation //
+
+// set up menu options
 const categories = {
   "Solar System": {
     "The Sun": ["Photosphere", "Chromosphere", "Corona"],
@@ -27,13 +31,19 @@ const categories = {
   },
 };
 
+
+// Functions and Handlers //
+
+// build sub-menu and fill options based on obj sub-category
 function fillMenu(category) {
   const isMobile = $('.header-nav ul').data('mobile');
   !isMobile && $('.menu-container').empty();
   isMobile && $('.menu-sub-mobile').empty();
   const divtype = (isMobile) ? "menu-div-mobile" : "menu-div";
   
+  // loop each category in categories obj
   for (let sub in categories[category]) {
+    // create new list container
     const menuElement = `
       <div class="${divtype}">
         <h1>${sub}</h1>
@@ -57,8 +67,8 @@ function fillMenu(category) {
   }
 }
 
-
-function buildMenu() {
+// build main menu options based on obj main categories
+function initMenu() {
   $('.header-nav ul, .menu-container-mobile ul').empty();
   const vw = window.innerWidth;
   const isMobile = (vw < 600) ? 1 : 0;
@@ -66,9 +76,9 @@ function buildMenu() {
   if (isMobile) {
     const element = `
       <li class="bar-container header-menu">
-        <svg width="24" height="24" viewBox="0 0 24 24">
-          <path class="bar" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
-        </svg>
+      <svg width="24" height="24" viewBox="0 0 24 24">
+        <path class="bar" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+      </svg>
       </li>
     `
     $('.header-nav ul').append(element);
@@ -88,36 +98,26 @@ function buildMenu() {
   }
 }
 
-function hideMenu() {
-  $('.menu-container').removeClass('show-menu');
-  $('.menu-container-mobile').removeClass('show-menu');
-  $('.menu-sub-mobile').removeClass('show-menu');
-}
-
-buildMenu();
-
-window.onresize = function(event) {
-  hideMenu();
-  buildMenu();
-};
-
-$('.header-nav ul').on('mouseenter', '.header-menu', function(event) {
+// handle header hover events
+function handleMouse(event, element) {
   const vw = window.innerWidth;
   const isMobile = (vw < 600) ? 1 : 0;
+
   if (!isMobile) {
     if ((event.type !== 'click' && event.type !== 'mouseenter') || !$('.menu-container').data('state')) {
       $('.menu-container').addClass('show-menu');
       $('.menu-container').data('state', 1);
-      fillMenu($(this).text());
+      fillMenu($(element).text());
       $('.menu-div').addClass('show-item');
     }
   }
-});
+}
 
-
-$('.header-nav ul').on('click', '.header-menu', function(event) {
+// handle header mobile click events
+function handleMobile() {
   const vw = window.innerWidth;
   const isMobile = (vw < 600) ? 1 : 0;
+
   if (isMobile) {
     $('.menu-container-mobile').toggleClass('show-menu');
     $('.menu-container-mobile li').removeClass('side');
@@ -126,46 +126,60 @@ $('.header-nav ul').on('click', '.header-menu', function(event) {
   }
 
   $('body').addClass('disabled');
+
   setTimeout(function() {
     $('body').removeClass('disabled');
   },500);
-});
+}
 
-$('.header-nav ul').on('mouseleave', '.header-menu', function(event) {
-  if (event.type === 'mouseleave') {
-    $('.menu-container').data('hold', 1);
-    $('.menu-container').data('state', 0);
-  }
-});
-
-$('.upper-nav-container li').on('click mouseleave', function(event) {
-  $('.menu-container').data('hold', 0);
-});
-
-$('.menu-container, .menu-container-mobile').on('mouseleave', function() {
-  $(this).removeClass('show-menu');
-});
-
-$('.menu-container, .menu-container-mobile').on('mouseleave', function() {
-  $(this).removeClass('show-menu');
-  $('.menu-sub-mobile').removeClass('show-menu');
-});
-
-$('.menu-sub-mobile').on('mouseenter', function() {
-  $('.menu-container-mobile').addClass('show-menu');
-  $('.menu-sub-mobile').addClass('show-menu');
-});
-
-$('.menu-container-mobile ul').on('click', '.header-menu', function() {
+// handle side menu display
+function handleSideMenu(element) {
   $('.menu-sub-mobile').addClass('show-menu');
   $('.menu-container-mobile li').addClass('darker');
-  $(this).removeClass('darker');
+  $(element).removeClass('darker');
   $('.menu-container-mobile li').addClass('side');
-  fillMenu($(this).text());
+  fillMenu($(element).text());
+}
+
+// hide all menus
+function hideMenu() {
+  $('.menu-container').removeClass('show-menu');
+  $('.menu-container-mobile').removeClass('show-menu');
+  $('.menu-sub-mobile').removeClass('show-menu');
+}
+
+// initalize menu
+initMenu();
+
+
+// Listeners //
+
+// resizing responsiveness
+window.onresize = function(event) {
+  hideMenu();
+  initMenu();
+};
+
+// main header hover (mouse)
+$('.header-nav ul').on('mouseenter', '.header-menu', function(event) {
+  handleMouse(event, this);
 });
 
-$('.header-image, .content-container').on('mouseenter', hideMenu);
+// main header click (mobile tap)
+$('.header-nav ul').on('click', '.header-menu', function() {
+  handleMobile();
+});
 
+// control header navigation hover events
+// state = menu is, 1: active 0: hidden
+$('.header-nav ul').on('mouseleave', '.header-menu', function(event) {
+    $('.menu-container').data('state', 0);
+});
 
-$(document).on('scroll', hideMenu);
+// mobile menu click
+$('.menu-container-mobile ul').on('click', '.header-menu', function() {
+  handleSideMenu(this);
+});
 
+// hide menu on focus loss
+$('.header-image, .content-container, .header-image').on('click scroll', hideMenu);
